02880954ba818805d3d788c55944869d0581f650
Sasu <sasuolander@yahoo.com>
2022-12-01 15:11:17 +0200

    fix jest configuration to work with module

--- test/helperlibrary.test.js
+++ test/helperlibrary.test.js
@@ -1,32 +1,68 @@
-describe("TC1: In SPPAC page when searching item, it returns what is searched.", () => {
+
+
+import isEmpty from "../helperlibrary/src/isEmpty"
+import isArrayLike from"../helperlibrary/src/isArrayLike"
+import countBy from"../helperlibrary/src/countBy"
+import filter from"../helperlibrary/src/filter"
+import words from"../helperlibrary/src/words"
+
+
+describe("isEmpty", () => {
+  test("Check if array is empty", () => {
+    const emptyArray = [] // prepare
+    expect(isEmpty(emptyArray)).toBe(true) // test
+  });
+
+  test("Array is not empty, Return false", () => {
+    const array = ["test"]  // prepare
+    expect(isEmpty(array)).toBe(false) // test
+  });
+  test("Array is not empty, more than one item, Return false", () => {
+    const array = ["test","test"] // prepare
+    expect(isEmpty(array)).toBe(false) // test
+  });
+});
+
+describe("isArrayLike", () => {
+
+
+  test("Check if it is array, return true", () => {
+    const array = [] // prepare
+    expect(isArrayLike(array)).toBe(true) // test
+  });
+  test("Check if it is array, two item in array, return true", () => {
+    const array = ["test","test"] // prepare
+    expect(isArrayLike(array)).toBe(true) // test
+  });
+
+  test("Check if it is array, object, return false", () => {
+    const object = {} // prepare
+    expect(isArrayLike(object)).toBe(false) // test
+  });
+
+});
+describe("countBy", () => {
   test("dummy", () => {
     expect(3).toBe(3);
   });
 });
-
-
-describe("TC2: In SPPAC page when searching item by using category, it returns what is searched.", () => {
-    test("dummy", () => {
-      expect(3).toBe(3);
-    });
+describe("filter", () => {
+  test("dummy", () => {
+    expect(3).toBe(3);
   });
-
-
-  describe("TC3:Customer can add product to cart in SPPAC pages.", () => {
-    test("dummy", () => {
-        expect(3).toBe(3);
-    });
+});
+describe("words", () => {
+  test("dummy", () => {
+    expect(3).toBe(3);
   });
-
-
-  describe("TC4: Customer can remove product from cart in SCP pages.", () => {
-    test("dummy", () => {
-        expect(3).toBe(3);
-    });
+});
+describe("isEmpty", () => {
+  test("dummy", () => {
+    expect(3).toBe(3);
   });
-
-  describe("TC5: SCP page calculate total cost of all products.", () => {
-    test("dummy", () => {
-        expect(3).toBe(3);
-    });
+});
+describe("isEmpty", () => {
+  test("dummy", () => {
+    expect(3).toBe(3);
   });
+});
