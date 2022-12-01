d857847885afd275718b766f52c0fff0477132c2
Sasu <sasuolander@yahoo.com>
2022-12-01 14:29:17 +0200

    add file helper library test

---
+++ test/testcasetest.test.js
@@ -0,0 +1,32 @@
+describe("TC1: In SPPAC page when searching item, it returns what is searched.", () => {
+  test("dummy", () => {
+    expect(3).toBe(3);
+  });
+});
+
+
+describe("TC2: In SPPAC page when searching item by using category, it returns what is searched.", () => {
+    test("dummy", () => {
+      expect(3).toBe(3);
+    });
+  });
+
+
+  describe("TC3:Customer can add product to cart in SPPAC pages.", () => {
+    test("dummy", () => {
+        expect(3).toBe(3);
+    });
+  });
+
+
+  describe("TC4: Customer can remove product from cart in SCP pages.", () => {
+    test("dummy", () => {
+        expect(3).toBe(3);
+    });
+  });
+
+  describe("TC5: SCP page calculate total cost of all products.", () => {
+    test("dummy", () => {
+        expect(3).toBe(3);
+    });
+  });
